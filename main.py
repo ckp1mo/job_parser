33123dc724f52d4e23ee60c60a8f20f84a8489ad
Alexandr Gritsunov <ickpimo@gmail.com>
2023-10-24 18:51:44 +0300

    file search optimization

--- main.py
+++ main.py
@@ -3,19 +3,24 @@ from src.vacancy import VacancyOptions, JSONSaver, Vacancy
 from pathlib import Path
 from utils.print_info import PrintInfo
 
-VACANCY_FILE = Path(Path.cwd(), 'output_files', 'vacancy.json')
-VACANCY_FILE_FILTERED = Path(Path.cwd(), 'output_files', 'vacancy_filtered.json')
+OUTPUT_FILES = Path(Path.cwd(), 'output_files')
 
 
 print('Привет!')
-if VACANCY_FILE.exists():
-    bool_ = get_started()
-    if bool_:
+file_list = []
+for files in OUTPUT_FILES.glob('*.json'):
+    if files.exists():
+        file_list.append(files.name)
+if len(file_list) > 0:
+    file_name = get_started(file_list)
+    file_path = Path(Path.cwd(), 'output_files', f'{file_name}')
+    print(file_path)
+    if file_name:
         while True:
             key_word = input('Введите ключевые слова для поиска по вакансиям: ').lower().split()
-            vacancy_list_by_key = VacancyOptions().get_by_key(VACANCY_FILE, key_word)
-            if len(vacancy_list_by_key) == 0:
-                print('Поиск по ключевым словам не удался')
+            vacancy_list_by_key = VacancyOptions().get_by_key(file_path, key_word)
+            if vacancy_list_by_key is None or len(vacancy_list_by_key) == 0:
+                print('>> Поиск по ключевым словам не удался\n')
                 user_repeat = input('Попробуем снова или начать новый поиск вакансий?'
                                     '\n1. Попробовать еще раз'
                                     '\n2. Начать новый поиск\n')
@@ -37,8 +42,6 @@ if VACANCY_FILE.exists():
                                 '\n3. Начать новый поиск'
                                 '\n4. Завершить программу\n')
             if user_repeat == '1':
-                if VACANCY_FILE_FILTERED.exists():
-                    VACANCY_FILE_FILTERED.unlink()
                 Vacancy.vacancy_to_instance(vacancy_list_by_key)
                 JSONSaver().save_vacancy(Vacancy.all)
                 print('Вакансии сохранены, ищи их в рабочей директори по пути /output_files/vacancy_filtered.json')
