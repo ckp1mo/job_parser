c38bf02363269d9a0cbc9f35d25c0587292cb34c
Alexandr Gritsunov <ickpimo@gmail.com>
2023-10-24 18:26:50 +0300

    deleted useless func

--- utils/utils.py
+++ utils/utils.py
@@ -1,26 +1,3 @@
-from pathlib import Path
-
-
-VACANCY_FILE = Path(Path.cwd(), 'output_files', 'vacancy.json')
-
-
-def get_from_api(xx_request_api, xx_vacancy, search_query):
-    """
-    Функция создает экземпляры для работы с классом xx_RequestApi и xx_Vacancy. Далее у экземпляров вызываются методы
-    xx_array - будущий список вакансий.
-    :param search_query: передается поисковой запрос
-    :param xx_request_api: передается класс для работы с API, где "хх" в имени подразумевает имя класса
-    для работы с платформой
-    :param xx_vacancy: передается класс для работы с массивом, где "хх" в имени подразумевает имя класса
-    для работы с платформой
-    :return: возвращает список вакансий -> list
-    """
-    #
-    request_api = xx_request_api(search_query)
-    xx_array = xx_vacancy(request_api.get_vacancies()).vacancy_to_list()
-    return xx_array
-
-
 def vacancy_filter(filter_words, vacancy_list):
     """
     Функция для фильтрации вакансий по ключевым словам
@@ -35,17 +12,6 @@ def vacancy_filter(filter_words, vacancy_list):
                 filtered_vacancy.append(copy)
                 break
     return filtered_vacancy
-
-
-def sort_vacancy_by_salary(vacancy_list, is_reverse):
-    """
-    Функция для сортировки вакансий по зарплате
-    :param is_reverse: булевое значение указывает будет ли использована реверсивная сортировка: bool
-    :param vacancy_list: список экземпляров класса Vacancy: list
-    :return: отсортированный список вакансий -> list
-    """
-    sort_vacancy = sorted(vacancy_list, key=lambda x: x.salary, reverse=is_reverse)
-    return sort_vacancy
 
 
 def get_top_vacancies(vacancy_list, top_n):
